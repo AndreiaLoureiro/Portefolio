07c558ee4c41337b971dd7840ec502165013a13e
Andreia Loureiro <180341723+AndreiaLoureiro@users.noreply.github.com>
2024-10-10 22:45:46 +0100

    primeiro commit com Nebeans

---
+++ src/excecao02/Excecao02.java
@@ -0,0 +1,26 @@
+
+package excecao02;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author andre
+ */
+public class Excecao02 {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.print("Digite um número: ");
+            String input = scanner.nextLine();
+            
+            // Tentativa de conversão
+            int numero = Integer.parseInt(input);
+            System.out.println("O número digitado é: " + numero);
+        } catch (NumberFormatException e) {
+            System.out.println("Erro: Você não digitou um número válido.");
+        }
+    }
+}
